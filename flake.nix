01976cec26e10978e8cf718f3d5ad22523a098ab
rhermens <royhermens@hotmail.com>
2025-07-07 07:10:43 +0200

    flake build

--- flake.nix
+++ flake.nix
@@ -3,16 +3,24 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        crane.url = "github:ipetkov/crane";
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, flake-utils }:
+    outputs = { self, nixpkgs, flake-utils, crane }:
         flake-utils.lib.eachDefaultSystem (system: 
-            let pkgs = nixpkgs.legacyPackages.${system};
-            in {
-                devShells.default = pkgs.mkShell {
-                    packages = [ pkgs.pkg-config pkgs.openssl ];
-                };
-            }
-        );
+            let 
+                pkgs = nixpkgs.legacyPackages.${system};
+                craneLib = crane.mkLib pkgs;
+            in
+        {
+            devShells.default = pkgs.mkShell {
+                packages = [ pkgs.pkg-config pkgs.openssl ];
+            };
+
+            packages.default = craneLib.buildPackage {
+                src = craneLib.cleanCargoSource ./.;
+                nativeBuildInputs = [ pkgs.pkg-config pkgs.openssl ];
+            };
+        });
 }
